4d91631bdf6a958d0eb0b80c5068e318711b0670
Manavi Sharma <150844157+manavi-24@users.noreply.github.com>
2024-08-17 23:44:42 +0530

    Time: 4 ms (56.95%), Space: 12.9 MB (73.52%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void findcombo(int ind, vector<int>& candidates, int target, vector<int>& ds, vector<vector<int>>& ans) {
+        if (target == 0) {
+            ans.push_back(ds);
+            return;
+        }
+        for (int i = ind; i < candidates.size(); i++) {
+         
+            if (i > ind && candidates[i] == candidates[i - 1]) continue;
+            if (candidates[i] > target) break; 
+            ds.push_back(candidates[i]);
+            findcombo(i + 1, candidates, target - candidates[i], ds, ans);
+            ds.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end()); 
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findcombo(0, candidates, target, ds, ans);
+        return ans;
+    }
+};
