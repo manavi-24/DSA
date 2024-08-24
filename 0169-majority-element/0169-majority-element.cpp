5c34824646936054fab99e9fac8b686bad1f2880
Manavi Sharma <150844157+manavi-24@users.noreply.github.com>
2024-08-24 23:44:08 +0530

    Time: 18 ms (52.79%), Space: 27 MB (45.45%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        int ele;
+        for (int i=0; i<n; i++){
+            if(count==0){
+                count=1;
+                ele=nums[i];
+            }
+            else if(ele==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+
+        }
+        return ele;
         
     }
 };
